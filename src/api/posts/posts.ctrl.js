a4384f394d512bfed6fe2ed40447e551616a055d
kasumil <ridou99@gmail.com>
2024-12-09 02:58:04 +0900

    [Add]몽고 디비 설정 완료

---
+++ src/api/posts/posts.ctrl.js
@@ -0,0 +1,85 @@
+import Post from '../../models/posts.js';
+
+/*
+  POST /api/posts
+  {
+    title: '제목',
+    body: "내용",
+    tags: ['태그1', '태그2']
+  }
+*/
+export const write = async (ctx) => {
+  const { title, body, tags } = ctx.request.body;
+  const post = new Post({
+    title,
+    body,
+    tags,
+  });
+  try {
+    await post.save();
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+/*
+  GET /api/posts
+*/
+export const list = async (ctx) => {
+  try {
+    const posts = await Post.find().exec();
+    ctx.body = posts;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+/*
+  GET /api/posts/:id
+*/
+export const read = async (ctx) => {
+  const { id } = ctx.params;
+
+  try {
+    const post = await Post.findById(id).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+/*
+  DELETE /api/posts/:id
+*/
+export const remove = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    await Post.findByIdAndDelete(id).exec();
+    ctx.status = 204;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+/*
+  PATCH /api/posts/:id
+*/
+export const update = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+      new: true,
+    }).exec();
+    if (!post) {
+      ctx.status = 404;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,35 @@
-const hello = 'hello';
-console.log(hello);
+import dotenv from 'dotenv';
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyParser';
+import mongoose from 'mongoose';
+import api from './api/index.js';
+
+dotenv.config();
+
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((e) => {
+    console.error(e);
+  });
+
+const app = new Koa();
+const router = new Router();
+
+router.use('/api', api.routes());
+
+// 라우터 적용전에 적용
+app.use(bodyParser());
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
