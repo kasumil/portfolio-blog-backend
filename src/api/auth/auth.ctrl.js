16c7adcc2d24deb2b93f207f2464be0cfcd7bde4
kasumil <ridou99@gmail.com>
2024-12-11 02:33:00 +0900

    [add]유저 로그인 및 로그아웃, 유저 로그인상태 확인 기능 추가

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -38,17 +38,77 @@ export const register = async (ctx) => {
 
     // 응답 데이터에서 hashedPassword 삭제
     ctx.body = user.serialize();
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
+      httpOnly: true,
+    });
   } catch (e) {
     ctx.throw(500, e);
   }
 };
 
+/*
+  Post /api/auth/login
+  {
+    username: 'ssh',
+    password: "mypass123"
+  }
+*/
 export const login = async (ctx) => {
   // 로그인
+  const { username, password } = ctx.request.body;
+
+  // username, password 없으면 에러처리
+  if (!username || !password) {
+    ctx.status = 401;
+    return;
+  }
+
+  try {
+    const user = await User.findByUsername(username);
+    // 계정이 존재하지 않으면 에러처리
+    if (!user) {
+      ctx.status = 401;
+      return;
+    }
+
+    const valid = await user.checkPassword(password);
+    // 비밀번호 틀림.
+    if (!valid) {
+      ctx.status = 401;
+      return;
+    }
+
+    ctx.body = user.serialize();
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
+      httpOnly: true,
+    });
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
+
+/*
+  GET /api/auth/check
+*/
 export const check = async (ctx) => {
   // 로그인상태 확인
+  const { user } = ctx.state;
+  if (!user) {
+    ctx.status = 401;
+    return;
+  }
+  ctx.body = user;
 };
+
+/*
+  POST /api/auth/logout
+*/
 export const logout = async (ctx) => {
   // 로그아웃
+  ctx.cookies.set('access_token');
+  ctx.status = 204; // No Content
 };
